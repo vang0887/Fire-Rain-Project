ea948e23acc4d13b6563c0c83dbd639b8284a3c4
vang0887 <vang0887@gmail.com>
2020-01-11 22:49:27 -0600

    updaing app and main files

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+import main
 
+#################################################
+# Flask Setup
+#################################################
 # Create an instance of Flask
 app = Flask(__name__)
 
+#################################################
+# PyMongo Connection Setup
+#################################################
 # Use PyMongo to establish Mongo connection
-"""mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")"""
+mongo = PyMongo(app, uri="mongodb://localhost:27017/WikiScrape")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,25 +20,19 @@ app = Flask(__name__)
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    caliFires = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", caliFires=caliFires)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-
-    # Run the scrape function
-    """costa_data = scrape_costa.scrape_info()"""
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
-
-    # Redirect back to home page
+    caliFires1 = mongo.db.califires
+    caliData = main.scrape_all()
+    mongo.db.collection.update({}, caliData, upsert=True)
     return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,49 +1,137 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 import time
 
+# def init_browser():
+# @NOTE: Replace the path with your actual path to the chromedriver
+executable_path = {"executable_path": "C:\chromedrv\chromedriver.exe"}
+Browser("chrome", **executable_path, headless=False)
 
-def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
+
+def caliCampScrape(browser):
+
     executable_path = {"executable_path": "C:\chromedrv\chromedriver.exe"}
-    return Browser("chrome", **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
 
-
-def scrape_info():
-    browser = init_browser()
-
-    # Visit visitcostarica.herokuapp.com
-    # url = "https://visitcostarica.herokuapp.com/" (need URL to Scrape wild fires)
+    url = "https://en.wikipedia.org/wiki/List_of_California_wildfires"
     browser.visit(url)
 
-    time.sleep(1)
-
-    # Scrape page into Soup
     html = browser.html
-    soup = bs(html, "html.parser")
+    soup = bs(html, 'html.parser')
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    img_url ="https://static01.nyt.com/images/2019/10/30/us/30fires-photos11/merlin_163584246_b9a52315-78b1-4255-906c-52e8952d9b6d-superJumbo.jpg?quality=90&auto=webp"
+    
+    para_text = soup.find('p').text
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
+    text_part1 = para_text.split("[1]")
+    text_part1[0]
+    
+    text_part2 = text_part1[1].splitlines()
+    text_part2 = text_part2[0].split("[2]")
+    text_part2[0]
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
+    intro_para = text_part1[0] + text_part2[0]
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    table = pd.read_html(url)
 
-    # Store data in a dictionary
-    # costa_data = {
-    #     "sloth_img": sloth_img,
-    #     "min_temp": min_temp,
-    #     "max_temp": max_temp
-    # }
+    des_fire_df= table[1]
 
-    # Close the browser after scraping
+    des_fire_df["Name"][0] = "Camp"
+
+    fire_table_html = des_fire_df.to_html()
+
+    facts_list = fire_table_html.splitlines()
+    separator = ""
+
+    fire_table_html = separator.join(facts_list)
+
+    mendo_fire_url = "https://i2.wp.com/www.mercurynews.com/wp-content/uploads/2019/10/Mendocino-Complex-1.jpg?fit=780%2C9999px&ssl=1"
+
+    camp_fire_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Camp_Fire_oli_2018312_Landsat.jpg/1280px-Camp_Fire_oli_2018312_Landsat.jpg"
+
+    return fire_table_html
+    
+def caliMendoScrape(browser):
+
+    executable_path = {"executable_path": "C:\chromedrv\chromedriver.exe"}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    mendo_url = 'https://www.cnn.com/2018/09/20/us/california-mendocino-complex-fire-contained-trnd/index.html'
+    browser.visit(mendo_url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    #Article Headline for Mendocino
+    headline = soup.find("h1").text
+
+    #scrape para1 for Mendocino
+    para1 = soup.find("div",class_="zn-body__paragraph").text
+
+    para=soup.find("div",class_="l-container").text
+
+    str1 = para.split(".")
+
+    para2 = str1[4] + "." + str1[5]
+
+    # Scrape para2 for Mendocina
+    para2
+
+
+    mendo_para = para1 + para2
+
+    return mendo_para
+
+
+def caliDeadliestScrape(browser):
+
+    executable_path = {"executable_path": "C:\chromedrv\chromedriver.exe"}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    camp_url = "https://abc7.com/the-deadliest-wildfires-in-california-history/4673982/"
+    browser.visit(camp_url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    #camp fire headline 
+    camp_headline = soup.find("h1").text
+
+    camp_headline
+
+    camp_para1 = soup.find("div",class_ = "body-text")
+
+    camp_para1_clean = camp_para1.text.split(".")[0] + ". " +  camp_para1.text.split(".")[1]
+
+    deadliest_element = browser.find_link_by_text("deadliest wildfires in California history").first
+
+    #scrape top20 deadliest wildfires in CA
+    # deadliest_element.click()
+
+    return camp_para1_clean,
+
+def scrape_all():
+    executable_path = {"executable_path": "C:\chromedrv\chromedriver.exe"}
+    browser = Browser("chrome", **executable_path)
+    fire_table_html = caliCampScrape(browser)
+    mendo_para = caliMendoScrape(browser)
+    camp_para1_clean = caliDeadliestScrape(browser)
+    # top20Fire = mars_facts()
+    # hemisphere_image_urls = hemisphere(browser)
+    # timestamp = dt.datetime.now()
+
+    data = {
+         "fire_table_html": fire_table_html,
+         "mendo_para": mendo_para,
+         "camp_para1_clean": camp_para1_clean,
+    #     "weather": mars_weather,
+    #     "facts": facts,
+    #     "hemispheres": hemisphere_image_urls,
+    #     "last_modified": timestamp
+     }
     browser.quit()
+    return data 
 
-    # Return results
-    # return costa_data
+if __name__ == "__main__":
+    print(scrape_all())
